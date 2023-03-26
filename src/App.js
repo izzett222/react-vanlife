6924d60412d754ea8f0ff1114e89cd699dd0a4eb
izzett222 <iizzeddin62@gmail.com>
2023-03-26 12:24:43 +0200

    add vans page and single van page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppLayout from './components/AppLayout';
 import About from './pages/About';
 import Home from './pages/Home';
+import Van from './pages/Van';
+import Vans from './pages/Vans';
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +11,8 @@ function App() {
       <Route element={<AppLayout/>}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<Van />} />
       </Route>
         
       </Routes>

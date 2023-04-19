c4353619de48a2227154fa1b068500eacb1ca2c6
izzett222 <iizzeddin62@gmail.com>
2023-04-19 12:18:54 +0200

    remove empty line in app.jsx and remove typescript in the tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path='/vans' element={<Vans />} />
         <Route path='/vans/:id' element={<Van />} />
-      </Route>
-        
+      </Route> 
       </Routes>
     </BrowserRouter>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
 
   theme: {
